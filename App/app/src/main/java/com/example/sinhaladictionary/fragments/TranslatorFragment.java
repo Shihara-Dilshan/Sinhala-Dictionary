3f8beb5b2c049a8d0245c26f478240a9eff16569
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-07-14 15:36:54 +0530

    translator functionality completed

--- App/app/src/main/java/com/example/sinhaladictionary/fragments/TranslatorFragment.java
+++ App/app/src/main/java/com/example/sinhaladictionary/fragments/TranslatorFragment.java
@@ -8,10 +8,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.sinhaladictionary.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TranslatorFragment extends Fragment {
+    private Button translateBtn;
+    private EditText inputText, translatedText;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -19,6 +36,16 @@ public class TranslatorFragment extends Fragment {
 
         ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
         View view = inflater.inflate(R.layout.fragment_translator, container, false);
+        translateBtn = view.findViewById(R.id.translate_btn);
+        inputText = view.findViewById(R.id.input_text);
+        translatedText = view.findViewById(R.id.result_text);
+        translateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputTextString = inputText.getText().toString();
+                translateText(inputTextString);
+            }
+        });
         return view;
     }
 
@@ -28,5 +55,67 @@ public class TranslatorFragment extends Fragment {
         setHasOptionsMenu(true);
     }
 
+    public void translateText(String inputText){
+        String postUrl = "https://baroque-chaise-29236.herokuapp.com/api/v2/translate";
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
 
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("input", inputText);
+            postData.put("lang1", "en");
+            postData.put("lang2", "si");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    translatedText.setText(response.getString("result"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+    public void volleyGet(){
+        String url = "http://127.0.0.1:5000/api/v2/get";
+        List<String> jsonResponses = new ArrayList<>();
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("data");
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String email = jsonObject.getString("email");
+                        System.out.println(email);
+                        jsonResponses.add(email);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+
+    }
 }
--- Backend/translateService/api/api.py
+++ Backend/translateService/api/api.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, make_response
 from services.api_service import translate_service
 ocr_route = Blueprint('ocr_route', __name__)
 
 @ocr_route.route("/api/v2/translate", methods=['POST'])
 def translate_route():
     data = request.get_json()
-    return translate_service(data)
+    result = translate_service(data)
+    return make_response({"result": result})
+
+@ocr_route.route("/api/v2/get", methods=['GET'])
+def translate_rsoute():
+    return "hi"
